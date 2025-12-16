58f8d826967d3397b714e7ae6582cedb36189e30
MRUJJWAL01 <chouhanujjwalsingh20@gmail.com>
2025-12-17 02:07:31 +0530

    dfgs

--- app/src/config/Axios.Intance.jsx
+++ app/src/config/Axios.Intance.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const AxiosIntance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL:"https://insta-clone-99rk.onrender.com/api",
     withCredentials:true
     
 }) 
-
